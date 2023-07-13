2ecb07e16759034c0583973f65c9019da54f112d
kerensp <105900888+kerensp@users.noreply.github.com>
2023-07-13 01:06:36 -0400

    preparando proyecto

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint-disable linebreak-style */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "@fontsource/poppins";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+      contrastText: "#4F2D80",
+    },
+    secondary: {
+      main: "#4F2D80",
+      contrastText: "#FFFFFF",
+    },
+  },
+  typography: {
+    fontSize: 18,
+    fontFamily: ["Poppins", "Segoe UI", "Roboto"].join(","),
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
