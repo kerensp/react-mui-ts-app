a1e1efd92051941c6c700ed14dd8654403391e54
kerensp <105900888+kerensp@users.noreply.github.com>
2023-07-13 16:40:01 -0400

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,12 @@
 /* eslint-disable linebreak-style */
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+
 import App from "./App";
 
-import "@fontsource/poppins";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FFFFFF",
-      contrastText: "#4F2D80",
-    },
-    secondary: {
-      main: "#4F2D80",
-      contrastText: "#FFFFFF",
-    },
-  },
-  typography: {
-    fontSize: 18,
-    fontFamily: ["Poppins", "Segoe UI", "Roboto"].join(","),
-  },
-});
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>, 
+  document.getElementById("root")
 );
