27eb2f1648498bf7a8f9c0871ab0768aec0cdf7d
kerensp <105900888+kerensp@users.noreply.github.com>
2023-07-14 16:12:56 -0400

    Adding some details

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -62,3 +62,18 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
     </>
   );
 };
+
+
+/*
+Este código exporta un componente llamado MenuLateral que utiliza el contexto DrawerContext para manejar el estado de un cajón lateral en una aplicación de React. El componente MenuLateral toma un prop children que se utiliza para renderizar el contenido de la página principal.
+
+Dentro del componente MenuLateral, se utiliza el hook useTheme y useMediaQuery de Material-UI para obtener el tema actual y la información de punto de quiebre de la pantalla. Luego, se utiliza useDrawerContext para obtener el estado isDrawerOpen y la función toggleDrawerOpen del contexto DrawerContext.
+
+El componente Drawer de Material-UI se utiliza para renderizar el cajón lateral. El prop open se establece en isDrawerOpen para controlar si el cajón se muestra o no. El prop variant se establece en temporary si la pantalla es pequeña (menor o igual que sm según el theme) y permanent de lo contrario. El prop onClose se establece en toggleDrawerOpen para que el cajón se cierre cuando se haga clic en el botón de cierre o se toque fuera del cajón.
+
+Dentro del cajón lateral, se utiliza el componente Box de Material-UI para establecer la anchura del cajón y el componente Avatar para mostrar una imagen de perfil. Se utiliza el componente Divider para separar la sección del avatar de la lista de elementos.
+
+La sección de la lista de elementos se establece dentro de un componente Box con una propiedad flex de 1 para que ocupe todo el espacio disponible en el cajón. Se utiliza el componente List y ListItemButton para renderizar un botón de lista con un ícono de HomeIcon y un texto Initial Page.
+
+Fuera del cajón lateral, se utiliza el componente Box para establecer la altura de la página principal en 100vh y la propiedad marginLeft en 0 si la pantalla es pequeña y en theme.spacing(28) de lo contrario. El prop children se utiliza para renderizar el contenido de la página principal.
+*/
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -35,5 +35,13 @@ export const DrawerProvider: React.FC<IDrawerProviderProps> = ({ children }) => 
 };
 
 /* 
+Este código es una implementación de un contexto en React que proporciona un estado compartido para el estado de un cajón (drawer) en una aplicación. Un contexto en React es una forma de pasar datos desde un componente padre a varios componentes hijos sin tener que pasar las props manualmente a través de cada nivel de la jerarquía de componentes.
 
+El código importa varias funciones de React, incluyendo createContext, useCallback, useContext y useState. createContext se utiliza para crear un nuevo contexto, useCallback se utiliza para crear una función de devolución de llamada que no se vuelve a crear en cada renderizado, useContext se utiliza para acceder al valor actual del contexto y useState se utiliza para crear un estado local para la variable isDrawerOpen.
+
+El DrawerContext creado con createContext se utiliza para proporcionar el valor del estado compartido a través del DrawerProvider. El useDrawerContext es un gancho personalizado que se utiliza para acceder al valor del DrawerContext en cualquier componente que esté dentro del árbol de componentes del DrawerProvider.
+
+El DrawerProvider es un componente de función que toma un prop children y proporciona el valor del contexto a sus componentes hijos. El estado local isDrawerOpen se inicializa en false y se actualiza mediante la función setIsDrawerOpen, que se pasa como argumento a useState. Se define una función toggleDrawerOpen mediante useCallback que invierte el valor actual del estado del cajón.
+
+Finalmente, el componente DrawerProvider envuelve a los componentes hijos con el contexto creado mediante DrawerContext.Provider. El valor del contexto se establece en un objeto con las claves isDrawerOpen y toggleDrawerOpen, que se pasan como valores a value en el Provider. De esta manera, cualquier componente hijo que llame a la función useDrawerContext puede acceder al estado del cajón y la función toggleDrawerOpen.
 */
