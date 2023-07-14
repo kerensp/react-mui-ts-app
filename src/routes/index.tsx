e35780eb03bb192c2805c08866fe7317d6c55537
kerensp <105900888+kerensp@users.noreply.github.com>
2023-07-14 16:01:54 -0400

    Transforming side menu into a responsive drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,17 @@
 import { Button } from '@mui/material';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext();
-
-  //La línea de código const { toggleTheme } = useAppThemeContext(); desestructura el objeto que devuelve el hook useAppThemeContext y obtiene la función toggleTheme del contexto del tema de la aplicación. De esta manera, la función toggleTheme se puede utilizar en el componente para cambiar el tema de la aplicación cuando sea necesario.
+  const { toggleDrawerOpen } = useDrawerContext();
 
   return (
     <Routes>
       <Route
         path='/initial-page'
         element={
-          <Button variant='contained' color='primary' onClick={toggleTheme}>
-            TOGGLE THEME
+          <Button variant='contained' color='primary' onClick={toggleDrawerOpen} >
+            TOGGLE DRAWER
           </Button>
         }
       />
@@ -21,14 +19,3 @@ export const AppRoutes = () => {
     </Routes>
   );
 };
-
-/* 
-La primera ruta, <Route path="/initial-page" element={<p>Initial Page</p>} />, establece una ruta para la página inicial de la aplicación. Cuando el usuario visita la URL "/initial-page", se renderiza un elemento de párrafo que dice "Initial Page". El elemento JSX <p> es el contenido que se muestra en la página.
-<Route path="/initial-page" element={<Button>Pruebas</Button>} /> boton que dice pruebas cuando se da click se cambia el tema
-
-La segunda ruta, <Route path="*" element={<Navigate to="/initial-page" />} />, establece una ruta para todas las demás URL que no coincidan con la primera ruta definida. El path * coincidirá con cualquier URL que no haya sido definida en otras rutas.
-
-Cuando el usuario visita una URL que no coincide con ninguna ruta definida, se renderiza un elemento <Navigate> que redirige al usuario a la ruta "/initial-page". El componente Navigate es parte de React Router y se utiliza para navegar programáticamente en la aplicación.
-
-En resumen, este código define dos rutas en una aplicación de React Router: una para la página inicial y otra para todas las demás URL no encontradas. Cuando el usuario visita una URL no encontrada, se redirige automáticamente a la página inicial.
-*/
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,17 @@
-import { Avatar, Box, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
+import { useDrawerContext } from '../../contexts';
 
 interface IMenuLateralProps {
   children: React.ReactNode;
@@ -8,13 +20,16 @@ interface IMenuLateralProps {
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme();
 
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  
   return (
     <>
-      <Drawer variant='permanent'>
-        <Box width={theme.spacing(28)} >
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+        <Box width={theme.spacing(28)} display='flex' flexDirection='column'>
           <Box
             width='100%'
-
             height={theme.spacing(20)}
             display='flex'
             alignItems='center'
@@ -41,7 +56,7 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
         </Box>
       </Drawer>
 
-      <Box height='100vh' marginLeft={theme.spacing(28)}>
+      <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
